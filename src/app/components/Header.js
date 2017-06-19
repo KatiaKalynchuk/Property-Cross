6ba7fd7039c205242dab849ef31d5690cea1c299
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-06-19 10:51:43 +0300

    Modifided a Header class

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -5,7 +5,11 @@ class Header extends Component {
     render() {
         return (
           <header className="header">
-        
+              <h1>Property Cross</h1>
+                  <button>Faves</button>
+                  <p>Use the form below to search for houses to buy. You can search by place-name, postcode, or 
+                    click 'My location', to search in your current location!
+                  </p>
           </header>
         );
     }
