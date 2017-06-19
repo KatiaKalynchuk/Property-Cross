3d811f9ac43402c13de17f3ae315043f4c9d85b0
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-06-19 10:48:36 +0300

    Modifided a MainSection class

--- src/app/components/MainSection.js
+++ src/app/components/MainSection.js
@@ -2,15 +2,40 @@ import React, {Component, PropTypes} from 'react';
 import Footer from './Footer';
 
 class MainSection extends Component {
+    constructor(props) {
+        super(props);
+        this.searchListings = this.searchListings.bind(this);
+    }
+    
+    searchListings () {
+        this.props.actions.search(this.searchInput.value);
+        this.searchInput.value = '';
+    }
 
     render() {
+        const {items} = this.props;
         return (
           <section className="main">
-                
+                <input placeholder = "Enter your location" ref={(input) => {this.searchInput = input;}}/>
+                <button onClick={this.searchListings}>Go</button>
+                <button id="location-btn">My location</button>
+                <label>Recent searches:</label>
+                <div className = "list">
+                    {
+                        items.map(item => {
+                            return <li>{item.title}</li>
+                        })
+                    }
+                </div>
           </section>
         );
     }
 }
+
+MainSection.propTypes = {
+    items: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
+};
 
 
 export default MainSection;
