45f6c36b8b5d90ddb907207fc5bc75d7e4766aca
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-06-22 10:53:15 +0300

    Add a Faves button

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,18 +1,16 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router';
 
-class Header extends Component {
-
-    render() {
-        return (
+const Header = () => {
+    return (
           <header className="header">
               <h1>Property Cross</h1>
-                  <button>Faves</button>
+                  <Link className="btn-faves" to="/faves">Faves</Link>
                   <p>Use the form below to search for houses to buy. You can search by place-name, postcode, or 
                     click 'My location', to search in your current location!
                   </p>
           </header>
-        );
-    }
+    );
 }
 
 export default Header;
