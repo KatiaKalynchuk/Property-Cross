6d5dfe36d9780779a6d954a178e35383999b308c
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-06-23 11:44:34 +0300

    Modified reducerData.

--- src/app/reducers/reducerData.js
+++ src/app/reducers/reducerData.js
@@ -1,8 +1,10 @@
-import {SEARCH, ERROR, ADD_TO_FAVES} from '../constants/ActionTypes';
+import {SEARCH, ERROR, ADD_TO_FAVES, REMOVE_FROM_FAVES, LOCATION, RECENT_SEARCHES} from '../constants/ActionTypes';
 
 const initialState = {
     searchData: [],
-    favesData: []
+    favesData: [],
+    locationData: [],
+    recentSearches: []
 };
 
 export default function reducerData(state = initialState, action) {
@@ -16,7 +18,24 @@ export default function reducerData(state = initialState, action) {
         return Object.assign({}, state, {
             favesData: [...state.favesData, action.payload]
         });
-      
+
+    case REMOVE_FROM_FAVES:
+        return Object.assign({}, state, {
+            favesData: state.favesData.filter(faves =>{
+                return Number(faves.latitude) !== Number(action.payload.latitude)
+            })
+        });
+
+    case LOCATION:
+        return Object.assign({}, state, {
+            locationData: [...action.payload]
+        });
+
+    case RECENT_SEARCHES: 
+        return Object.assign({}, state, {
+            recentSearches: [...state.recentSearches, action.payload] 
+        });   
+
     default: 
         return state;
     }
