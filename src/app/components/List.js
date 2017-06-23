0d24fff7f80920abf7f5080aa303352169ddd03f
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-06-23 11:31:20 +0300

    Modified List.js

--- src/app/components/List.js
+++ src/app/components/List.js
@@ -5,20 +5,10 @@ import {Link} from 'react-router';
 import Item from './item';
 
 class List extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            path: '/list'
-        }
-    }
-
     render() {
-        const {items, view} = this.props;
+        const {items} = this.props;
         return (
             <div className="result-page">
-                <div className={view.preloader ? 'show' : 'hide'}>
-                    <img src="./../../spin.gif"></img>
-                </div>
                 <header className="list-header">
                     <p><Link to="/"><img src="./../../back.svg"></img>Property Cross</Link></p>
                     <h2>Results</h2>
@@ -28,7 +18,7 @@ class List extends Component {
                     {
                         items.map((item) => {
                             return (
-                                <Item item={item} key={shortid.generate()} path={this.state.path}/>
+                                <Item item={item} key={shortid.generate()}/>
                             )
                         })
                     }
@@ -39,8 +29,7 @@ class List extends Component {
 }
 
 List.propTypes = {
-    items: PropTypes.array.isRequired,
-    view: PropTypes.object.isRequired
+    items: PropTypes.array.isRequired
 };
 
 export default List;
