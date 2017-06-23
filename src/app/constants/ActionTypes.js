963f357de418c9d8df562aa0ea57803c1d75585a
Katia Kalynchuk <katerynakalynchuk@gmail.com>
2017-06-23 11:39:11 +0300

    Modified ActionsTypes. Added LOCATION, REMOVE_FROM_FAVES, RECENT_SEARCHES

--- src/app/constants/ActionTypes.js
+++ src/app/constants/ActionTypes.js
@@ -2,3 +2,6 @@ export const SEARCH = 'SEARCH';
 export const PRELOADER = 'PRELOADER';
 export const ERROR = 'ERROR';
 export const ADD_TO_FAVES = 'ADD_TO_FAVES';
+export const LOCATION = 'LOCATION';
+export const REMOVE_FROM_FAVES = 'REMOVE_TO_FAVES';
+export const RECENT_SEARCHES = 'RECENT_SEARCHES';
